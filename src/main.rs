454b3613ead9cfdb8db1fa308b6917a3f590bc90
ergXned <126957013+ergXned@users.noreply.github.com>
2025-02-18 11:03:58 +0100

    main: refoctor for better visability

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,8 @@ to flash: espflash flash ./target/riscv32imc-esp-espidf/release/esp32_rustboard 
 */
 
 use anyhow;
-use ble::BleStatus;
 use esp32_rustboard::*;
 use esp_idf_hal::task::block_on;
-use heapless::FnvIndexMap;
-use spin::Mutex;
-
-use crate::config::config::*;
-use crate::debounce::*;
-use crate::matrix::{scan_grid, Key};
 
 fn main() -> anyhow::Result<()> {
     esp_idf_svc::sys::link_patches();
@@ -20,23 +13,30 @@ fn main() -> anyhow::Result<()> {
     /* Bind the log crate to the ESP Logging facilities */
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    /* initialize keys pressed hashmap */
-    let keys_pressed: Mutex<FnvIndexMap<Key, Debounce, PRESSED_KEYS_INDEXMAP_SIZE>> =
-        Mutex::new(FnvIndexMap::new());
-
-    /* ble connection information shared variable */
-    let ble_status: Mutex<BleStatus> = Mutex::new(BleStatus::NotConnected);
-
     #[cfg(feature = "master")]
     {
         use crate::ble::master::ble_rx_tx;
+        use crate::config::config::*;
+        use crate::debounce::*;
+        use crate::matrix::Key;
+        use ble::BleStatus;
         use embassy_futures::select::select3;
+        use heapless::FnvIndexMap;
+        use matrix::scan_grid_master;
+        use spin::Mutex;
+
+        /* initialize keys pressed hashmap */
+        let keys_pressed: Mutex<FnvIndexMap<Key, Debounce, PRESSED_KEYS_INDEXMAP_SIZE>> =
+            Mutex::new(FnvIndexMap::new());
+
+        /* ble connection information shared variable */
+        let ble_status: Mutex<BleStatus> = Mutex::new(BleStatus::NotConnected);
 
         /* run the tasks concurrently */
         block_on(async {
             select3(
                 ble_rx_tx(&keys_pressed, &ble_status),
-                scan_grid(&keys_pressed, &ble_status),
+                scan_grid_master(&keys_pressed, &ble_status),
                 calculate_debounce(&keys_pressed),
             )
             .await;
@@ -45,13 +45,13 @@ fn main() -> anyhow::Result<()> {
 
     #[cfg(feature = "slave")]
     {
-        use crate::ble::slave::ble_tx;
-        use embassy_futures::select::select;
+        use matrix::scan_grid_slave;
 
         /* run the tasks concurrently */
         block_on(async {
-            select(ble_tx(&ble_status), scan_grid(&keys_pressed, &ble_status)).await;
+            scan_grid_slave().await;
         });
     }
+
     Ok(())
 }
