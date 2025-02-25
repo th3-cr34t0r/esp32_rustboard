e90344105c136c668513962477ba09c204420018
ergXned <126957013+ergXned@users.noreply.github.com>
2025-02-25 16:06:18 +0100

    main: separated the rx and tx parts for the master device

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,6 @@ use crate::debounce::*;
 use crate::matrix::Key;
 use alloc::sync::Arc;
 use ble::BleStatus;
-use embassy_futures::select::select3;
 use heapless::FnvIndexMap;
 use matrix::scan_grid;
 use spin::Mutex;
@@ -33,14 +32,20 @@ fn main() -> anyhow::Result<()> {
 
     #[cfg(feature = "master")]
     {
-        use crate::ble::master::ble_rx_tx;
+        use crate::ble::master::{ble_rx, ble_tx};
+        use ble::BleKeyboardMaster;
+        use embassy_futures::select::select4;
 
         /* run the tasks concurrently */
         block_on(async {
-            select3(
-                ble_rx_tx(&keys_pressed, &ble_status),
+            let ble_keyboard: Arc<Mutex<BleKeyboardMaster>> =
+                Arc::new(Mutex::new(BleKeyboardMaster::new().await));
+
+            select4(
                 scan_grid(&keys_pressed, &ble_status),
                 calculate_debounce(&keys_pressed),
+                ble_rx(&ble_keyboard, &keys_pressed),
+                ble_tx(&ble_keyboard, &ble_status, &keys_pressed),
             )
             .await;
         });
@@ -49,13 +54,14 @@ fn main() -> anyhow::Result<()> {
     #[cfg(feature = "slave")]
     {
         use crate::ble::slave::ble_tx;
+        use embassy_futures::select::select3;
 
         /* run the tasks concurrently */
         block_on(async {
             select3(
-                ble_tx(&keys_pressed, &ble_status),
                 scan_grid(&keys_pressed, &ble_status),
                 calculate_debounce(&keys_pressed),
+                ble_tx(&keys_pressed, &ble_status),
             )
             .await;
         });
