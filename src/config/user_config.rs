887a5936c6e295527ad6120b32b769907831b988
ergXned <126957013+ergXned@users.noreply.github.com>
2025-04-22 13:50:06 +0200

    config: fix slave build

--- src/config/user_config.rs
+++ src/config/user_config.rs
@@ -23,8 +23,6 @@ pub const COL_INIT: u8 = 0;
 pub const DEBOUNCE_DELAY: Duration = Duration::from_millis(20);
 #[cfg(feature = "master")]
 pub const ESP_POWER_LEVEL: EspPowerLevel = EspPowerLevel::Negative0;
-#[cfg(feature = "master")]
-pub const FAST_CURSOR_VALUE: u8 = 5;
 
 #[cfg(feature = "slave")]
 pub const COL_INIT: u8 = 6;
@@ -34,6 +32,7 @@ pub const DEBOUNCE_DELAY: Duration = Duration::from_millis(5);
 pub const ESP_POWER_LEVEL: EspPowerLevel = EspPowerLevel::Negative0;
 
 pub const LAYERS: usize = 3;
+pub const FAST_CURSOR_VALUE: u8 = 5;
 pub const BLE_STATUS_DEBOUNCE_DELAY: Duration = Duration::from_millis(500); //0.5 sec
 pub const SLEEP_DELAY: Duration = Duration::from_millis(600000); //10 minutes
 pub const SLEEP_DELAY_NOT_CONNECTED: Duration = Duration::from_millis(60000); //1 minute
